398c7e2fd9ec81c2cd09b3654aa83c9399829a29
Dang Trung Hieu <hieudangtrung@protonmail.com>
2024-05-20 13:45:25 +0800

    Add Output Stack and forecast

--- components/OutputStack.py
+++ components/OutputStack.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from torch.nn.modules.pixelshuffle import PixelShuffle
 from torch.nn.utils.parametrizations import spectral_norm
 
 from LatentConditioningStack import LatentConditioningStack
@@ -9,9 +10,9 @@ from blocks.LBlock import LBlock
 from blocks.SpatialAttention import SpatialAttention
 
 
-class ConvGRUStack(nn.Module):
+class OutputStack(nn.Module):
     """
-    ConvGRUStack for Sampler implementation from https://arxiv.org/abs/2104.00954
+    OutputStack for Sampler implementation from https://arxiv.org/abs/2104.00954
 
     Args:
         `return_hidden`: bool
@@ -24,32 +25,31 @@ class ConvGRUStack(nn.Module):
     Examples:
 
     >>> h0 = [torch.zeros((5, 768, 8, 8)), torch.zeros((5, 384, 16, 16)), torch.zeros((5, 192, 32, 32)), torch.zeros((5, 96, 64, 64))]
-    >>> output, h0 = ConvGRUStack()(h)
+    >>> output, h = OutputStack()(h0)
+    >>> output.shape
+    torch.Size([5, 1, 256, 256])
     """
 
     def __init__(self, return_hidden=True):
         super().__init__()
         self.return_hidden = return_hidden
-
         self.convGRUs = nn.ModuleList(
-            [ConvGRU(384, 768), ConvGRU(192, 384), ConvGRU(96, 192)]
+            [ConvGRU(384, 768), ConvGRU(192, 384), ConvGRU(96, 192), ConvGRU(48, 96)]
         )
-        self.convGRU_4 = ConvGRU(48, 96)
-
         self.conv1s = nn.ModuleList(
             [
                 spectral_norm(nn.Conv2d(768, 768, 1, groups=768)),
                 spectral_norm(nn.Conv2d(384, 384, 1, groups=384)),
                 spectral_norm(nn.Conv2d(192, 192, 1, groups=192)),
+                spectral_norm(nn.Conv2d(96, 96, 1, groups=96)),
             ]
         )
-        self.conv1_4 = spectral_norm(nn.Conv2d(96, 96, 1, groups=96))
-
         self.gblocks = nn.ModuleList(
             [
                 GBlock(768, 768, upsampling=False),
                 GBlock(384, 384, upsampling=False),
                 GBlock(192, 192, upsampling=False),
+                GBlock(96, 96, upsampling=False),
             ]
         )
         self.gblocks_up = nn.ModuleList(
@@ -57,8 +57,13 @@ class ConvGRUStack(nn.Module):
                 GBlock(768, 384, upsampling=True),
                 GBlock(384, 192, upsampling=True),
                 GBlock(192, 96, upsampling=True),
+                GBlock(96, 48, upsampling=True),
             ]
         )
+        self.bn = nn.BatchNorm2d(48)
+        self.relu = nn.ReLU()
+        self.conv1_5 = spectral_norm(nn.Conv2d(48, 4, 1))
+        self.d2s = PixelShuffle(upscale_factor=2)
 
     def forward(self, prev_state):
         new_state = []
@@ -78,10 +83,11 @@ class ConvGRUStack(nn.Module):
             x = G(x)
             x = G_up(x)
 
-        x = self.convGRU_4(prev_state[-1], x)
-        new_state.append(x)
-        output_state = self.conv1_4(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.conv1_5(x)
+        x = self.d2s(x)
 
         if not self.return_hidden:
-            return output_state
-        return (output_state, new_state)
+            return x
+        return (x, new_state)
