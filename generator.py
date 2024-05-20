5f280bcf6a1d22d2112c6207f10810f31ad74949
Dang Trung Hieu <hieudangtrung@protonmail.com>
2024-05-20 17:17:43 +0800

    First commit

---
+++ generator.py
@@ -0,0 +1,60 @@
+import sys
+sys.path.append('components')
+# sys.path.append('components/blocks')
+
+import torch
+import torch.nn as nn
+
+from components.ConditioningStack import ConditioningStack
+from components.OutputStack import OutputStack
+
+
+class Sampler(nn.Module):
+    def __init__(self, forecast_steps=18):
+        super().__init__()
+        self.output_stacks = nn.ModuleList(
+            [OutputStack() for _ in range(forecast_steps)]
+        )
+
+    def forward(self, h):
+        out = []
+        for output_stack in self.output_stacks:
+            output, h = output_stack(h)
+            out.append(output)
+        
+        out = torch.cat(out, dim=1)
+        return out
+
+
+class Generator(nn.Module):
+    """
+    Generator implementation from https://arxiv.org/abs/2104.00954
+
+    Args:
+        `forecast_steps`: `int`
+            number of forecast steps. Default: 18
+
+    Shape:
+        - Input: (N, T, 1, W, H)
+        - Output: (N, 18, 1, W, H)
+
+    Examples:
+
+    >>> input = torch.zeros((5, 4, 1, 256, 256))
+    >>> output = Generator(18)(input)
+    >>> output.shape
+    torch.Size([5, 18, 1, 256, 256])
+    """
+
+    def __init__(self, forecast_steps=18):
+        super().__init__()
+        self.cond_stack = ConditioningStack()
+        self.sampler = Sampler(forecast_steps)
+
+
+    def forward(self, x):
+        h = self.cond_stack(x)
+        out = self.sampler(h)
+        return out
+
+
