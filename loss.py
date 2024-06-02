d2159c38b4a71a4ffdfba23eb7d1c6cf1086e7e0
Dang Trung Hieu <hieudangtrung@protonmail.com>
2024-06-02 12:11:51 +0800

    use torch.clamp

--- loss.py
+++ loss.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn.functional as F
 
 from components.Generator import Generator
@@ -13,7 +14,7 @@ temp_dis = TemporalDiscriminator()
 spa_dis = SpatialDiscriminator()
 
 
-def regularization(pred, obs, N=N, H=H, W=W):
+def regularization(pred, obs):
     """
     Grid cell regularization term implementation from https://arxiv.org/abs/2104.00954
 
@@ -22,12 +23,6 @@ def regularization(pred, obs, N=N, H=H, W=W):
             predictions, outputs from the Generator
         `obs`:`torch.Tensor`
             actual observation corresponding to `pred`
-        `N`:`int`
-            lead time. Default: 18
-        `H`:`int`
-            image height. Default: 256
-        `W`: `int`
-            image width. Default: 256
 
     Shape:
         - pred: (N, T, 1, W, H) or (N, T, W, H)
@@ -35,25 +30,19 @@ def regularization(pred, obs, N=N, H=H, W=W):
 
     Examples:
 
-    >>> input = torch.zeros((5, 4, 1, 256, 256))
-    >>> obs = torch.zeros((5, 4, 1, 256, 256))
+    >>> input = torch.zeros((5, 12, 1, 256, 256))
+    >>> obs = torch.zeros((5, 12, 1, 256, 256))
     >>> pred = Generator(18)(input)
     >>> regularization(obs, preds)
     """
 
-    if pred.ndim == 4:
-        pred = pred[:, :, None, :, :]
-    w = lambda y: max(y + 1, 24)
-    l = (pred - obs) * (obs.apply_(w))
-    lr = l.mean() / (N * H * W)
+    lr = torch.mean(torch.abs(pred - obs) * torch.clamp(pred + 1, 24))
     return lr
 
 
-def G_loss(pred, obs, temp_pred, spa_pred, l=20, N=N, H=H, W=W):
-    return (
-        temp_pred.mean()
-        + spa_pred.mean()
-        - l * regularization(pred, obs, N=N, H=H, W=W)
+def G_loss(pred, obs, T_pred, S_pred, lambda_=20):
+    return torch.abs(
+        T_pred.mean() + S_pred.mean() - lambda_ * regularization(pred, obs)
     )
 
 
