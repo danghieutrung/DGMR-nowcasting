133cfe150c646d9ca2880944873cf4fdd1e092e8
Dang Trung Hieu <hieudangtrung@protonmail.com>
2024-06-02 12:22:24 +0800

    use seperate optimizers

--- trainer_example.py
+++ trainer_example.py
@@ -1,12 +1,11 @@
 from datetime import datetime
-from torch.utils.tensorboard import SummaryWriter
-
-
+import time
 import torch
 from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torch.nn import MSELoss, L1Loss
 
 from loss import *
-from model import DGMRNowcasting
 
 
 X_train, y_train = torch.zeros((2, 4, 1, 256, 256)), torch.zeros((2, 18, 1, 256, 256))
@@ -21,62 +20,69 @@ batch_size = 2
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-model = DGMRNowcasting()
-optimizer = torch.optim.Adam(model.parameters())
+temporal_discriminator = TemporalDiscriminator()
+spatial_discriminator = SpatialDiscriminator()
+generator = Generator()
+
+optimizerT = torch.optim.Adam(
+    temporal_discriminator.parameters(), lr=0.0002, betas=(0, 0.999)
+)
+optimizerS = torch.optim.Adam(
+    spatial_discriminator.parameters(), lr=0.0002, betas=(0, 0.999)
+)
+optimizerG = torch.optim.Adam(generator.parameters(), lr=0.00005, betas=(0, 0.999))
+
+mse_loss, l1_loss = MSELoss(), L1Loss()
 
 
-def train_one_epoch(epoch_index, tb_writer):
-
+def train_one_epoch():
     running_gloss, running_tloss, running_sloss = 0.0, 0.0, 0.0
-    last_gloss, last_tloss, last_sloss = 0.0, 0.0, 0.0
 
     for i, train_data in enumerate(train_loader):
 
         # Every data instance is an input + obs pair
         X_train, y_train = train_data
+        pred = generator(X_train)
+        pred_all, obs_all = torch.cat((X_train, pred.detach()), dim=1), torch.cat(
+            (X_train, y_train), dim=1
+        )
 
-        # Zero your gradients for every batch!
-        optimizer.zero_grad()
-
-        # Make predictions for this batch
-        pred, T_pred, S_pred = model(X_train)
-
-        # Compute the losses and their gradients
-        g_loss = G_loss(pred, y_train, T_pred, S_pred)
-        print("GG", g_loss)
-        g_loss.backward()
-        optimizer.step()
-
-        optimizer.zero_grad()
-        _, T_pred, _, T_obs, _ = model(X_train, y_train)
+        # Train temporal discriminator
+        temporal_discriminator.zero_grad()
+        T_pred, T_obs = temporal_discriminator(pred_all), temporal_discriminator(
+            obs_all
+        )
         t_loss = T_loss(T_pred, T_obs)
-        print("TT", t_loss)
         t_loss.backward()
-        optimizer.step()
+        optimizerT.step()
 
-        optimizer.zero_grad()
-        _, _, S_pred, _, S_obs = model(X_train, y_train)
+        # Train spatial discriminator
+        spatial_discriminator.zero_grad()
+        S_pred, S_obs = spatial_discriminator(pred.detach()), spatial_discriminator(
+            y_train
+        )
         s_loss = S_loss(S_pred, S_obs)
-        print("SS", s_loss)
         s_loss.backward()
-        optimizer.step()
+        optimizerS.step()
+
+        # Train generator
+        generator.zero_grad()
+        T_pred, S_pred = temporal_discriminator(pred_all), spatial_discriminator(
+            pred_all
+        )
+        g_loss = G_loss(pred, y_train, T_pred, S_pred)
+        g_loss.backward()
+        optimizerG.step()
 
         # Gather data and report
         running_gloss += g_loss.item()
         running_tloss += t_loss.item()
         running_sloss += s_loss.item()
 
-        if i % 1000 == 999:
-            last_gloss = running_gloss / 1000  # loss per batch
-            last_tloss = running_tloss / 1000
-            last_sloss = running_sloss / 1000
-
-            print("  batch {} GEN loss: {}".format(i + 1, last_gloss))
-            tb_x = epoch_index * len(train_loader) + i + 1
-            tb_writer.add_scalar("Loss/train", last_gloss, tb_x)
-            running_gloss, running_tloss, running_sloss = 0.0, 0.0, 0.0
-
-    return last_gloss, last_tloss, last_sloss
+    avg_gloss = running_gloss / (i + 1)
+    avg_tloss = running_tloss / (i + 1)
+    avg_sloss = running_sloss / (i + 1)
+    return avg_gloss, avg_tloss, avg_sloss
 
 
 def main():
@@ -84,45 +90,76 @@ def main():
     writer = SummaryWriter("runs/fashion_trainer_{}".format(timestamp))
     epoch_number = 0
 
-    EPOCHS = 1
+    EPOCHS = 60
 
     best_gloss = 1_000_000.0
 
     for epoch in range(EPOCHS):
-        print("EPOCH {}:".format(epoch_number + 1))
+        t = time.time()
+        print("EPOCH {}:".format(epoch + 1))
 
         # Make sure gradient tracking is on, and do a pass over the data
-        model.train(True)
+        temporal_discriminator.train(True)
+        spatial_discriminator.train(True)
+        generator.train(True)
 
-        avg_gloss_train, avg_tloss_train, avg_sloss_train = train_one_epoch(
-            epoch_number, writer
-        )
+        avg_gloss_train, avg_tloss_train, avg_sloss_train = train_one_epoch()
 
         running_gloss_test, running_tloss_test, running_sloss_test = 0.0, 0.0, 0.0
+        running_mse, running_l1 = 0.0, 0.0
         # Set the model to evaluation mode, disabling dropout and using population
         # statistics for batch normalization.
-        model.eval()
+        temporal_discriminator.eval()
+        spatial_discriminator.eval()
+        generator.eval()
 
         # Disable gradient computation and reduce memory consumption.
         with torch.no_grad():
             for i, test_data in enumerate(test_loader):
                 X_test, y_test = test_data
-                pred, T_pred, S_pred, T_obs, S_obs = model(X_test, y_test)
-                g_loss_test = G_loss(pred, y_test, T_pred, S_pred)
-                t_loss_test = T_loss(T_pred, T_obs)
-                s_loss_test = S_loss(S_pred, S_obs)
 
-                running_gloss_test += g_loss_test
-                running_tloss_test += t_loss_test
-                running_sloss_test += s_loss_test
+                pred = generator(X_test)
+                pred_all, obs_all = torch.cat(
+                    (X_test, pred.detach()), dim=1
+                ), torch.cat((X_test, y_test), dim=1)
+
+                T_pred, T_obs = temporal_discriminator(
+                    pred_all
+                ), temporal_discriminator(obs_all)
+                S_pred, S_obs = spatial_discriminator(pred_all), spatial_discriminator(
+                    obs_all
+                )
+
+                t_loss = T_loss(T_pred, T_obs)
+                s_loss = S_loss(S_pred, S_obs)
+                g_loss = G_loss(pred, y_test, T_pred, S_pred)
+                l2 = mse_loss(pred, y_test)
+                l1 = l1_loss(pred, y_test)
+
+                running_tloss_test += t_loss
+                running_sloss_test += s_loss
+                running_gloss_test += g_loss
+                running_mse += l2
+                running_l1 += l1
 
         avg_gloss_test = running_gloss_test / (i + 1)
         avg_tloss_test = running_tloss_test / (i + 1)
         avg_sloss_test = running_sloss_test / (i + 1)
+        avg_mse = running_mse / (i + 1)
+        avg_l1 = running_l1 / (i + 1)
 
-        print("GEN LOSS train {} test {}".format(avg_gloss_train, avg_gloss_test))
-        print("TEMP LOSS train {} test {}".format(avg_tloss_train, avg_tloss_test))
-        print("SPA LOSS train {} test {}".format(avg_sloss_train, avg_sloss_test))
+        print(
+            "GEN LOSS train {:.4f} test {:.4f}".format(avg_gloss_train, avg_gloss_test)
+        )
+        print(
+            "TMP LOSS train {:.4f} test {:.4f}".format(avg_tloss_train, avg_tloss_test)
+        )
+        print(
+            "SPA LOSS train {:.4f} test {:.4f}".format(avg_sloss_train, avg_sloss_test)
+        )
+        print("MSE {:.4f} L1 {:.4f}".format(avg_mse, avg_l1))
+        print("Epoch {}: {}s".format(epoch + 1, int(time.time() - t)))
+        # break
 
         # Log the running loss averaged per batch
         # for both training and validation
@@ -136,8 +173,18 @@ def main():
         # Track best performance, and save the model's state
         if avg_gloss_test < best_gloss:
             best_gloss = avg_gloss_test
-            model_path = "model_{}_{}".format(timestamp, epoch_number)
-            torch.save(model.state_dict(), model_path)
+            generator_path = "saved_models/generator/model_{}_{}_{}".format(
+                best_gloss, timestamp, epoch_number
+            )
+            torch.save(generator.state_dict(), generator_path)
+            tdis_path = "saved_models/temporal_discriminator/model_{}_{}".format(
+                timestamp, epoch_number
+            )
+            torch.save(temporal_discriminator.state_dict(), tdis_path)
+            sdis_path = "saved_models/spatial_discriminator/model_{}_{}".format(
+                timestamp, epoch_number
+            )
+            torch.save(spatial_discriminator.state_dict(), sdis_path)
 
         epoch_number += 1
 
